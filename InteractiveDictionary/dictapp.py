d42b15ef5997e788e1148096346353dd6858d5d5
cybervoid <4731255+cybervoid@users.noreply.github.com>
2017-08-26 11:10:18 -0400

    Added word matching algorithm

--- InteractiveDictionary/dictapp.py
+++ InteractiveDictionary/dictapp.py
@@ -1,17 +1,32 @@
 import json
-
+import difflib #library to try to match words with approximate names
+from difflib import SequenceMatcher
+from difflib import get_close_matches
 #help(json.load)
 data = json.load(open("data.json"))
 type(data)
 #print(data["rain"])
 
 def translate(w):
-    w = w.ToLower()
+    w = w.lower()
     if w in data:
-        return data[w]
+        return w + ": " + data[w]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        key = get_close_matches(w, data.keys())[0]
+        print("Did you mean %s instead? [Y]es" % key)
+        useri = input()
+        if(useri == "y"):
+            return data[key]
+        else:
+            return "Word not found."
     else: #error handling for missing word
-        return "Word does not exist. Please check again."
+        return w + ": " + "Word does not exist. Please check again."
 
 word = input("Enter word: ")
 
 print(translate(word))
+
+#Sequence matcher user:
+#SequenceMatcher(None, "rainn", "rain").ratio()
+#get_close_matches
+#help(get_close_matches) in terminal to view arguments
